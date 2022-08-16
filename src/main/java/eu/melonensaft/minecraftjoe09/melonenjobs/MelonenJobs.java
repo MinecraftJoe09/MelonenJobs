5ef74b3fd58e8cb6c3f8fed095b7fd206e29f452
MinecraftJoe09 <portschy.alexander@gmail.com>
2022-08-16 19:58:36 +0200

    Initial commit

---
+++ src/main/java/eu/melonensaft/minecraftjoe09/melonenjobs/MelonenJobs.java
@@ -0,0 +1,17 @@
+package eu.melonensaft.minecraftjoe09.melonenjobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MelonenJobs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
